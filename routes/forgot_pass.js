c8a8f5dbc642bf1009296106fafc478afc7a3e95
ashrah38 <hamza.ashrafch38@gmail.com>
2020-09-09 06:29:29 +0500

    added env variables

--- routes/forgot_pass.js
+++ routes/forgot_pass.js
@@ -14,8 +14,8 @@ let transporter = nodemailer.createTransport({
   port: 465,
   secure: true,
   auth: {
-    user: process.env.GMAIL_USER,
-    pass: process.env.GMAIL_PASS,
+    user: "cricketmatchmaker@gmail.com",
+    pass: "Hamzaggmu3!",
   },
   tls: {
     rejectUnauthorized: false,
